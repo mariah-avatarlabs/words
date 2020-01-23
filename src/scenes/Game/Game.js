f89c8208147114b758d677afa6bfb6a8bdbfdec0
Mariah Weathersby <mariah.weathersby@a1107.local>
2020-01-23 12:12:08 -0800

    game scene cleanup

--- src/scenes/Game/Game.js
+++ src/scenes/Game/Game.js
@@ -2,22 +2,13 @@ import 'phaser';
 import data from '../../assets/data/data.json';
 import * as util from '../../utilities/Utilities'
 
-import { WordController } from './classes/WordController';
-import { Timer } from './classes/Timer';
-import { Score } from './classes/hud/score/Score';
-import { WordBank } from './classes/WordBank';
 import { Grid } from './classes/Grid';
-import { ActiveWordDisplay } from './classes/ActiveWordDisplay';
 import { HUD } from './classes/hud/HUD.js'
  
 
 export default class Game extends Phaser.Scene {
   constructor () {
     super('Game');
-
-    this.activeWordDisplay = null;
-    this.letterGridArr = [];
-    this.wordBank = null;
 
     this.gameData = data;
     this.dataIndx = 0;
@@ -30,35 +21,24 @@ export default class Game extends Phaser.Scene {
     if(this.dataIndx + 1 < this.gameData.length){
       this.dataIndx++;
 
-      initLvl(this.dataIndx);
+      this.initLvl(this.dataIndx);
 
-      // this.word = this.gameData[this.dataIndx].word;
-      // this.question = this.gameData[this.dataIndx].question;
-      
-      // this.wordController.update(this.word);
-      // let newString = this.wordController.string;
-      // this.grid.updateGrid(newString);
     }
 
   }
 
   initLvl(dataIndx){
     let lvlData = this.gameData[dataIndx];
-    let lvlWordLetters = util.generateWordLetterSet(lvlData.word);
 
-    this.word = util.shuffleString(lvlWordLetters);
+    this.word = lvlData.word;
     this.question = lvlData.question;
 
-    this.grid.updateGrid(this.word);
+    let lvlWordLetters = util.generateWordLetterSet(this.word);
+    this.grid.updateGrid(util.shuffleString(lvlWordLetters));
 
   }
 
   submitWord(){
-
-      // submit new word to wordset
-      // this.wordController.submitNewWord(this.activeWordDisplay.currentWord);
-      console.log('targetWord: ', this.word);
-  
       if(this.hud.currentWord() === this.word){
         this.nextLvl();
 
@@ -67,41 +47,24 @@ export default class Game extends Phaser.Scene {
         this.hud.clearWordDisplay();
       }    
 
-
-
-
   }
  
   create () {
 
-
-    // // create score display
-    // this.score = new Score(this, 150, 150)
-
-    // // create current word display
-    // this.activeWordDisplay = new ActiveWordDisplay(this, 0, 0, this.question);
-    // this.activeWordDisplay.init();
-
     // create HUD
     this.hud = new HUD(this, 0, 0);
     this.hud.init();
-
 
     // create grid obj
     // refactor - expense calculation .getBounds??
     this.grid = new Grid(this, 0, this.hud.getBounds().height);
     this.grid.init(); 
 
-    console.log('grid: ', this.grid)
-
+    // hydrate with current level data
     this.initLvl(this.dataIndx);
 
-
-    // init event emitters
-    // this.initEventListeners();
-
-
-
+    // assign eventListeners
+    this.initEventListeners();
 
   }
 
@@ -109,7 +72,7 @@ export default class Game extends Phaser.Scene {
     document.addEventListener('keypress', (e) => {
       if (e.key === 'Enter') {
         //submit current word in bank
-        this.submitWord(this.activeWord);
+        this.submitWord(this.hud.currentWord());
 
       }
     })
@@ -120,31 +83,10 @@ export default class Game extends Phaser.Scene {
     this.word = this.gameData[this.dataIndx].word;
     this.question = this.gameData[this.dataIndx].question;
 
-    // create Wordbank for level
-    // this.wordBank = new WordBank(this, 0, 400);
-    // this.wordBank.init();
-
-    // config wordController - famecon
-    this.wordController = new WordController(this.word);
-    // this.wordController.init();
-    // console.log(this.wordController);
-   
-    // // create timer
-    // this.timer = new Timer(this, 270, 150 );
-    // this.timer.init();
-
-    // this.letterLvlArray = this.wordController.lettersArr;
-
-
-
-  }
-
-  addLetter(letter){
-
   }
 
   udpateActiveWord(letter){
-    this.hud.updateWord(letter);
+    this.hud.updateWordDisplay(letter);
   }
 
 
